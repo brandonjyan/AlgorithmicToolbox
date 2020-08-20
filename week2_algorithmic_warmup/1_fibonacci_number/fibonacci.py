d1a9ff0bfd9fc24353c6357fd580159bd341bd3e
Brandon Yan <yanjbrandon@gmail.com>
2020-08-20 12:31:11 -0700

    Update fibonacci.py

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,23 @@
-# Uses python3
-def calc_fib(n):
-    if (n <= 1):
+# python3
+
+def fibonacci_number_naive(n):
+    assert 0 <= n <= 45
+
+    if n <= 1:
         return n
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    return fibonacci_number_naive(n - 1) + fibonacci_number_naive(n - 2)
 
-n = int(input())
-print(calc_fib(n))
+def fibonacci_number(n):
+    if n <= 1:
+        return n
+
+    previous, current = 0, 1
+    for _ in range(n - 1):
+        previous, current = current, (previous + current)
+
+    return current
+
+if __name__ == '__main__':
+    input_n = int(input())
+    print(fibonacci_number(input_n))
