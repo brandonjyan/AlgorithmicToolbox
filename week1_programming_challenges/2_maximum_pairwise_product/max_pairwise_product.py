961d5673ad819c1fed2a1226779634afc8f1b3e6
Brandon Yan <yanjbrandon@gmail.com>
2020-08-20 12:32:08 -0700

    Update max_pairwise_product.py

--- week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -1,6 +1,5 @@
 # python3
 
-
 def max_pairwise_product(numbers):
     n = len(numbers)
     max_product = 0
@@ -11,8 +10,9 @@ def max_pairwise_product(numbers):
 
     return max_product
 
-
 if __name__ == '__main__':
     input_n = int(input())
     input_numbers = [int(x) for x in input().split()]
     print(max_pairwise_product(input_numbers))
+
+    
