aeef53385e3721234cc91dc27561b9dae25186c6
Brandon Yan <yanjbrandon@gmail.com>
2020-08-20 12:39:53 -0700

    Update fibonacci_partial_sum.py

--- week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
+++ week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
@@ -1,22 +1,45 @@
-# Uses python3
-import sys
+# python3
 
-def fibonacci_partial_sum_naive(from_, to):
-    sum = 0
+def last_digit_of_the_sum_of_fibonacci_numbers_again_naive(from_index, to_index):
+    assert 0 <= from_index <= to_index <= 10 ** 18
 
-    current = 0
-    next  = 1
+    if to_index == 0:
+        return 0
 
-    for i in range(to + 1):
-        if i >= from_:
-            sum += current
+    fibonacci_numbers = [0] * (to_index + 1)
+    fibonacci_numbers[0] = 0
+    fibonacci_numbers[1] = 1
+    for i in range(2, to_index + 1):
+        fibonacci_numbers[i] = fibonacci_numbers[i - 2] + fibonacci_numbers[i - 1]
 
-        current, next = next, current + next
+    return sum(fibonacci_numbers[from_index:to_index + 1]) % 10
 
-    return sum % 10
+
+def last_digit_of_the_sum_of_fibonacci_numbers_again(from_index, to_index):
+    assert 0 <= from_index <= to_index <= 10 ** 18
+
+    return (last_digit_of_the_sum_of_fibonacci_numbers(to_index) + 10 - \
+    last_digit_of_the_sum_of_fibonacci_numbers(from_index - 1)) % 10
+
+
+def last_digit_of_the_sum_of_fibonacci_numbers(n):
+    # 60 is the period length of the fibonacci function modulo 10
+    return (fibonacci_number((n + 2) % 60) + 9) % 10
+
+
+def fibonacci_number(n):
+    if n <= 1:
+        return n
+
+    previous, current = 0, 1
+    for _ in range(n - 1):
+        previous, current = current, (previous + current)
+
+    return current
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read();
-    from_, to = map(int, input.split())
-    print(fibonacci_partial_sum_naive(from_, to))
+    input_from, input_to = map(int, input().split())
+    print(last_digit_of_the_sum_of_fibonacci_numbers_again(input_from, input_to))
+
+    
