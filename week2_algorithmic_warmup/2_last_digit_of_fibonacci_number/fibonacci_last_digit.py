f60e339cf881e7f3b0ea05b0e88ba4f6763a99ee
Brandon Yan <yanjbrandon@gmail.com>
2020-08-20 12:33:40 -0700

    Update fibonacci_last_digit.py

--- week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -1,19 +1,31 @@
-# Uses python3
-import sys
+# python3
 
-def get_fibonacci_last_digit_naive(n):
+def last_digit_of_fibonacci_number_naive(n):
+    assert 0 <= n <= 10 ** 7
+
     if n <= 1:
         return n
 
-    previous = 0
-    current  = 1
+    return (last_digit_of_fibonacci_number_naive(n - 1) + last_digit_of_fibonacci_number_naive(n - 2)) % 10
+
+def last_digit_of_fibonacci_number(n):
+    assert 0 <= n <= 10 ** 7
+
+    if n <= 1:
+        return n
+
+    first = 0
+    second = 1
 
     for _ in range(n - 1):
-        previous, current = current, previous + current
+        temp = first
+        first = second % 10
+        second = (temp + second) % 10
 
-    return current % 10
+    return second
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    print(get_fibonacci_last_digit_naive(n))
+    input_n = int(input())
+    print(last_digit_of_fibonacci_number(input_n))
+    
+    
