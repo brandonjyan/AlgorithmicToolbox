31a78055c715a880bad0d20ee5f22db0160d85df
Brandon Yan <yanjbrandon@gmail.com>
2020-08-20 12:36:56 -0700

    Update fibonacci_huge.py

--- week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -1,19 +1,45 @@
-# Uses python3
-import sys
+# python3
 
-def get_fibonacci_huge_naive(n, m):
+def fibonacci_number_again_naive(n, m):
+    assert 0 <= n <= 10 ** 18 and 2 <= m <= 10 ** 3
+
     if n <= 1:
         return n
 
-    previous = 0
-    current  = 1
-
+    previous, current = 0, 1
     for _ in range(n - 1):
-        previous, current = current, previous + current
+        previous, current = current, (previous + current) % m
 
-    return current % m
+    return current
+
+def fibonacci_number_again(n, m):
+    assert 0 <= n <= 10 ** 18 and 2 <= m <= 10 ** 3
+
+    return fibonacci_number(n % period_length(m)) % m
+
+def fibonacci_number(n):
+    if n <= 1:
+        return n
+
+    previous, current = 0, 1
+    for _ in range(n - 1):
+        previous, current = current, (previous + current)
+
+    return current
+
+def period_length(m):
+    assert 2 <= m <= 10 ** 3
+
+    previous = current = -1
+    counter = 0
+    while previous != 0 or current != 1:
+        previous, current = current, (fibonacci_number(counter + 2) % m)
+        counter += 1
+
+    return counter
 
 if __name__ == '__main__':
-    input = sys.stdin.read();
-    n, m = map(int, input.split())
-    print(get_fibonacci_huge_naive(n, m))
+    input_n, input_m = map(int, input().split())
+    print(fibonacci_number_again(input_n, input_m))
+    
+    
