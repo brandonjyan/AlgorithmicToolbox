5ee248e5139c4c96904e43617ce547a3f2a1a104
Brandon Yan <yanjbrandon@gmail.com>
2020-08-20 12:39:11 -0700

    Update fibonacci_sum_last_digit.py

--- week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
+++ week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
@@ -1,21 +1,39 @@
-# Uses python3
-import sys
+# python3
 
-def fibonacci_sum_naive(n):
+def last_digit_of_the_sum_of_fibonacci_numbers_naive(n):
+    assert 0 <= n <= 10 ** 18
+
     if n <= 1:
         return n
 
-    previous = 0
-    current  = 1
-    sum      = 1
+    fibonacci_numbers = [0] * (n + 1)
+    fibonacci_numbers[0] = 0
+    fibonacci_numbers[1] = 1
+    for i in range(2, n + 1):
+        fibonacci_numbers[i] = fibonacci_numbers[i - 2] + fibonacci_numbers[i - 1]
 
+    return sum(fibonacci_numbers) % 10
+
+
+def last_digit_of_the_sum_of_fibonacci_numbers(n):
+    assert 0 <= n <= 10 ** 18
+
+    # 60 is the period length of the fibonacci function modulo 10
+    return (fibonacci_number((n + 2) % 60) + 9) % 10
+
+
+def fibonacci_number(n):
+    if n <= 1:
+        return n
+
+    previous, current = 0, 1
     for _ in range(n - 1):
-        previous, current = current, previous + current
-        sum += current
+        previous, current = current, (previous + current)
 
-    return sum % 10
+    return current
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    print(fibonacci_sum_naive(n))
+    input_n = int(input())
+    print(last_digit_of_the_sum_of_fibonacci_numbers(input_n))
+    
