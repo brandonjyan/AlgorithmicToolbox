403f67a870f15c53bf94be37c96787dbc4f46c5a
Brandon Yan <yanjbrandon@gmail.com>
2020-08-23 23:07:33 -0700

    Update binary_search.py

--- week4_divide_and_conquer/1_binary_search/binary_search.py
+++ week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -1,22 +1,52 @@
-# Uses python3
-import sys
+# python3
 
-def binary_search(a, x):
-    left, right = 0, len(a)
-    # write your code here
 
-def linear_search(a, x):
-    for i in range(len(a)):
-        if a[i] == x:
+def linear_search(keys, query):
+    for i in range(len(keys)):
+        if keys[i] == query:
             return i
+
     return -1
 
+
+def binary_search(keys, query):
+    # assert all(keys[i] < keys[i + 1] for i in range(len(keys) - 1))
+    # assert 1 <= len(keys) <= 3 * 10 ** 4
+
+    min = 0
+    max = len(keys) - 1
+
+    while min <= max:
+        mid = min + (max - min) // 2
+        if query == keys[mid]:
+            return mid
+        elif query < keys[mid]:
+            max = mid - 1
+        else:
+            min = mid + 1
+
+    return -1
+
+
+def binary_search_iterative(a, x):
+    left, right = 0, len(a) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+        if a[mid] == x:
+            return mid
+        elif x < a[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
+
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n = data[0]
-    m = data[n + 1]
-    a = data[1 : n + 1]
-    for x in data[n + 2:]:
-        # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+    input_keys = list(map(int, input().split()))[1:]
+    input_queries = list(map(int, input().split()))[1:]
+
+    for q in input_queries:
+        print(binary_search(input_keys, q), end=' ')
+
+        
