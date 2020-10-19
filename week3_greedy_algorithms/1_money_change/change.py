0e748031493c5f01d34235c66e3de3b7c33671d1
Brandon Yan <yanjbrandon@gmail.com>
2020-10-18 22:48:41 -0700

    Update change.py

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -1,10 +1,18 @@
-# Uses python3
-import sys
+# python3
 
-def get_change(m):
-    #write your code here
-    return m
+def money_change(money):
+    assert 0 <= money <= 10 ** 3
+    counter = 0
+
+    if money >= 10:
+        counter += money // 10
+        money %= 10
+    if money >= 5:
+        counter += money // 5
+        money %= 5
+
+    return counter + money
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+    input_money = int(input())
+    print(money_change(input_money))
