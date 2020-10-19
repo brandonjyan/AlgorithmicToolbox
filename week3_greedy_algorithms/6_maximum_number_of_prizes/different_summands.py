23be8b39dabc7a93ee3d93dcd3e4f184a88e0171
Brandon Yan <yanjbrandon@gmail.com>
2020-10-18 22:51:08 -0700

    Update different_summands.py

--- week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
+++ week3_greedy_algorithms/6_maximum_number_of_prizes/different_summands.py
@@ -1,15 +1,22 @@
-# Uses python3
-import sys
+# python3
 
-def optimal_summands(n):
+def compute_optimal_summands(n):
+    assert 1 <= n <= 10 ** 9
     summands = []
-    #write your code here
+    i = 1
+
+    while n >= 2 * i + 1:
+        summands.append(i)
+        n -= i
+        i += 1
+    if n > 0:
+        summands.append(n)
+
     return summands
 
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    summands = optimal_summands(n)
-    print(len(summands))
-    for x in summands:
-        print(x, end=' ')
+    input_n = int(input())
+    output_summands = compute_optimal_summands(input_n)
+    print(len(output_summands))
+    print(*output_summands)
