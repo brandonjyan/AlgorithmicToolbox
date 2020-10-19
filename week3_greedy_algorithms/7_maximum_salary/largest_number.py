03b90d97c797ef370a4c8c0a9303ad53a3704253
Brandon Yan <yanjbrandon@gmail.com>
2020-10-18 23:38:28 -0700

    Update largest_number.py

--- week3_greedy_algorithms/7_maximum_salary/largest_number.py
+++ week3_greedy_algorithms/7_maximum_salary/largest_number.py
@@ -1,17 +1,39 @@
-#Uses python3
+# python3
 
-import sys
+from itertools import permutations
 
-def largest_number(a):
-    #write your code here
-    res = ""
-    for x in a:
-        res += x
-    return res
+
+def largest_number_naive(numbers):
+    numbers = list(map(str, numbers))
+
+    largest = 0
+
+    for permutation in permutations(numbers):
+        largest = max(largest, int("".join(permutation)))
+
+    return largest
+
+
+def compare(digit1, digit2):
+    if int(str(digit1) + str(digit2)) > int(str(digit2) + str(digit1)):
+        return digit1
+    else:
+        return digit2
+
+
+def largest_number(numbers):
+    answer = ""
+    while len(numbers) > 0:
+        maxDigit = 0
+        for digit in numbers:
+            maxDigit = compare(maxDigit, digit)
+        numbers.remove(maxDigit)
+        answer += str(maxDigit)
+    return answer
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = input.split()
-    a = data[1:]
-    print(largest_number(a))
-    
+    n = int(input())
+    input_numbers = input().split()
+    assert len(input_numbers) == n
+    print(largest_number(input_numbers))
